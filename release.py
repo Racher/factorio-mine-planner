cacfca93f428126df024bd5da913975517189754
phimes <himespeter@gmail.com>
2020-07-19 01:46:51 +0200

    release.py

---
+++ release.py
@@ -0,0 +1,16 @@
+import os
+import json
+import shutil
+info=json.loads(open('info.json').read())
+folder=info['name']+'_'+info['version']
+try:
+    os.mkdir(folder)
+except:
+    pass
+files=['info.json','control.lua','thumbnail.png']
+for file in files:
+    shutil.copy(file,os.path.join(folder,file))
+try:
+    os.system('cmd /k \"C:\\Program Files\\7-Zip\\7zG.exe\" a -tzip '+folder+'.zip '+folder)
+except:
+    print('zip failed')
